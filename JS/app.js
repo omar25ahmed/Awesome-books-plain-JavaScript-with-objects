94545648972f9932ab6775220bd5be17cc67e0a2
Omar Ragheb <ahmed.raph29@gmail.com>
2022-02-08 00:39:16 +0200

    Adding Event Listeners and setting the local storage and setting linters up

--- JS/app.js
+++ JS/app.js
@@ -1,35 +1,36 @@
 let books = [];
-
-function book(title, author) {
+function Book(title, author) {
   this.title = title;
   this.author = author;
 }
 
-const deleteBook = function(title, author) {
+const deleteBook = function (title, author) {
   books = books.filter((book) => book.title !== title || book.author !== author);
-}
+};
 
-const setLocalStorage = function(books) {
+const setLocalStorage = (books) => {
   const data = JSON.stringify(books);
   localStorage.setItem('books', data);
-}
+};
 
- const bookContainer = document.querySelector('book-container');
+const bookContainer = document.querySelector('.book-container');
 
- function showBook(books) {
-  const bookDiv = document.createElement('div');
-  const titleBook = document.createElement('p');
-  const authorBook = document.createElement('p');
-  const deleteBtn = document.createElement('button');
+function showBook(book) {
+  const bookDiv = document.createElement('DIV');
+  const titleBook = document.createElement('P');
+  const authorBook = document.createElement('P');
+  const deleteBtn = document.createElement('BUTTON');
   const hr = document.createElement('hr');
+  deleteBtn.textContent = 'Remove';
 
-  deleteBtn.textContent('Delete');
-  titleBook.textContent(book.title);
-  authorBook.textContent(book.author);
+  titleBook.textContent = book.title;
+  authorBook.textContent = book.author;
+
   bookDiv.appendChild(titleBook);
   bookDiv.appendChild(authorBook);
   bookDiv.appendChild(deleteBtn);
   bookDiv.appendChild(hr);
+
   bookContainer.appendChild(bookDiv);
 
   deleteBtn.addEventListener('click', () => {
@@ -37,4 +38,37 @@ const setLocalStorage = function(books) {
     setLocalStorage(books);
     bookContainer.removeChild(bookDiv);
   });
- }
+}
+
+function iterateBooks() {
+  books.forEach((book) => {
+    showBook(book);
+  });
+}
+
+const inpTitle = document.querySelector('#title');
+const inpAuthor = document.querySelector('#author');
+
+function saveBooks() {
+  const book = new Book(inpTitle.value, inpAuthor.value);
+  books.push(book);
+  console.log(books);
+  setLocalStorage(books);
+  showBook(book);
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  saveBooks();
+  inpTitle.value = '';
+  inpAuthor.value = '';
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('books') !== null) {
+    const myBooks = JSON.parse(localStorage.getItem('books'));
+    books = myBooks;
+  }
+  iterateBooks();
+});
