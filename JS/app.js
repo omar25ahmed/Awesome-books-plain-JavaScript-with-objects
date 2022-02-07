bef820fc2379af6ccaa4fddf54254eb947ae78e8
Thi-Ponce <thi.pponce@gmail.com>
2022-02-07 19:24:06 -0300

    Js app

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,40 @@
-class Books {
-  constructor(title, author){
-    this.title = title;
-    this.author = author;
-  }
+let books = [];
+
+function book(title, author) {
+  this.title = title;
+  this.author = author;
 }
 
+const deleteBook = function(title, author) {
+  books = books.filter((book) => book.title !== title || book.author !== author);
+}
+
+const setLocalStorage = function(books) {
+  const data = JSON.stringify(books);
+  localStorage.setItem('books', data);
+}
+
+ const bookContainer = document.querySelector('book-container');
+
+ function showBook(books) {
+  const bookDiv = document.createElement('div');
+  const titleBook = document.createElement('p');
+  const authorBook = document.createElement('p');
+  const deleteBtn = document.createElement('button');
+  const hr = document.createElement('hr');
+
+  deleteBtn.textContent('Delete');
+  titleBook.textContent(book.title);
+  authorBook.textContent(book.author);
+  bookDiv.appendChild(titleBook);
+  bookDiv.appendChild(authorBook);
+  bookDiv.appendChild(deleteBtn);
+  bookDiv.appendChild(hr);
+  bookContainer.appendChild(bookDiv);
+
+  deleteBtn.addEventListener('click', () => {
+    deleteBook(book.title, book.author);
+    setLocalStorage(books);
+    bookContainer.removeChild(bookDiv);
+  });
+ }
